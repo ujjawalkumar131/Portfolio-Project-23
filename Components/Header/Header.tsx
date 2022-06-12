d0382e3f5c68b116e9d61a5c121a928ee1d0d8bd
Lohit244 <lohit244@gmail.com>
2022-06-12 16:48:25 +0530

    changed animation to start at opacity 0.1

--- Components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.css"
-
-export default function Header(){
-  return (
-  <div className={styles.HeaderContainer}>
-    Header
-  </div>
-  )
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Header from '../Components/Header/Header'
 import Footer from '../Components/Footer/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
