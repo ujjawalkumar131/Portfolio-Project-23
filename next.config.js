1aab66aa388d37a2df7b2cf9d97863457b545f5e
Lohit244 <lohit244@gmail.com>
2022-06-20 14:31:13 +0530

    google seo attempt

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/google4f094853163ee0a3',
+        destination: '/google4f094853163ee0a3.html',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
