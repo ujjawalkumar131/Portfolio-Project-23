8c8aa7868fcf037584e8fcbaba50be5b8673fe82
lohit244 <lohit244@gmail.com>
2022-06-27 19:54:07 +0530

    fix tailwind... turns out i was in the wrong

--- pages/_middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-// redirect old netlify deploy to new vercel deploy
-export async function middleware(req: NextRequest) {
-    if(req.nextUrl.hostname.toLowerCase().includes("netlify")){
-      // return NextResponse.redirect("https://lohit244.vercel.app")
-    }
-    return NextResponse.next()
-}
