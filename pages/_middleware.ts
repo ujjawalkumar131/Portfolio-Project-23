3f352246005b8dfe179b16a68ad388f44d064594
lohit244 <lohit244@gmail.com>
2022-06-27 17:29:30 +0530

    🙃

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from 'next/server'
 // redirect old netlify deploy to new vercel deploy
 export async function middleware(req: NextRequest) {
     if(req.nextUrl.hostname.toLowerCase().includes("netlify")){
-      return NextResponse.redirect("https://lohit244.vercel.app")
+      // return NextResponse.redirect("https://lohit244.vercel.app")
     }
     return NextResponse.next()
 }
