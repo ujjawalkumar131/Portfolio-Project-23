9bc7867aa7e8a868fccec4116f6a2aad5d8aea23
lohit244 <lohit244@gmail.com>
2022-06-27 16:31:36 +0530

    footer

--- Components/Footer/Footer.tsx
+++ Components/Footer/Footer.tsx
@@ -1,13 +1,10 @@
 import Connect from "../Connect/Connect"
 import OverlappingText from "../OverlappingText/OverlappingText"
-import styles from "./styles.module.css"
 
 export default function Footer(){
   return (
-  <div className={styles.FooterContainer}>
-    <div className="connect">
-      <OverlappingText backgroundtext="Connect" toptext="Wanna Reach Out?" />
-    </div>
+  <div className="text-center my-6">
+    <OverlappingText backgroundtext="Connect" toptext="Wanna Reach Out?" />
     <Connect/>
     © 2022 Lohitaksha Malhotra
   </div>
