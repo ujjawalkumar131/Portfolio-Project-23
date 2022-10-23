5e03a6c20aecf98f15a81c29970340b4ce59e1a2
lohit244 <lohit244@gmail.com>
2022-10-23 23:50:35 +0530

    feat: updated projects card with *new animation*

--- src/components/Landing/ProjectsSection/ProjectCard.tsx
+++ src/components/Landing/ProjectsSection/ProjectCard.tsx
@@ -1,56 +1,80 @@
-import Image, { StaticImageData } from "next/image"
-import Link from "next/link"
-import styles from "./styles.module.css"
-import {useInView} from "react-intersection-observer"
-import {animated, useSpring} from "react-spring"
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import styles from "./styles.module.css";
+import { useInView } from "react-intersection-observer";
+import { animated, useSpring } from "react-spring";
 
 type props = {
-  image: StaticImageData,
-  title: string,
-  content: string,
-  github: string,
-  projecturl: string,
-  reverse?: boolean,
-  tags: string[]
-}
+  image: StaticImageData;
+  title: string;
+  content: string;
+  github: string;
+  projecturl: string;
+  reverse?: boolean;
+  tags: string[];
+};
 
-export default function ProjectCard({image, title, content, github, projecturl, reverse=false, tags}:props){
-
+export default function ProjectCard({
+  image,
+  title,
+  content,
+  github,
+  projecturl,
+  reverse = false,
+  tags,
+}: props) {
   const [ref, inView] = useInView({
     threshold: 0.4,
-    triggerOnce: true
-  })
+    triggerOnce: true,
+  });
 
-  const fadeImage= useSpring({x: inView?0:20, opacity: inView?1:0, delay: 450});
-  const fade= useSpring({x: inView?0:20, opacity: inView?1:0, delay: 150});
+  const fadeImage = useSpring({
+    x: inView ? 0 : 20,
+    opacity: inView ? 1 : 0,
+    delay: 450,
+  });
+  const fade = useSpring({
+    x: inView ? 0 : 20,
+    opacity: inView ? 1 : 0,
+    delay: 150,
+  });
   return (
-    <div ref={ref} className={styles.CardContainer} style={reverse?{flexDirection: "row-reverse"}:{flexDirection: "row"}}>
+    <div
+      ref={ref}
+      className={styles.CardContainer}
+      style={
+        reverse ? { flexDirection: "row-reverse" } : { flexDirection: "row" }
+      }
+      onMouseMove={(e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        e.currentTarget.style.setProperty("--circle-x", `${x}px`);
+        e.currentTarget.style.setProperty("--circle-y", `${y}px`);
+      }}
+    >
       <animated.div style={fadeImage} className={styles.imageContainer}>
-      <Image src={image} alt="project screenshot"/>
+        <Image src={image} alt="project screenshot" />
       </animated.div>
       <animated.div style={fade} className={styles.right}>
-        <div className={styles.title}>
-          {title}
-        </div>
-        <div className={styles.content}>
-          {content}
-        </div>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.content}>{content}</div>
         <div className={styles.tags}>
-          {tags.map(tag=><div className={styles.tag} key={tag}>{tag}</div>)}
+          {tags.map((tag) => (
+            <div className={styles.tag} key={tag}>
+              {tag}
+            </div>
+          ))}
         </div>
         <div className={styles.links}>
           <Link href={github}>
-          <div className={styles.github}>
-            Source
-          </div>
+            <div className={styles.github}>Source</div>
           </Link>
           <Link href={projecturl}>
-          <div className={styles.projectUrl}>
-            Link
-          </div>
+            <div className={styles.projectUrl}>Link</div>
           </Link>
         </div>
       </animated.div>
     </div>
-  )
+  );
 }
--- src/components/Landing/ProjectsSection/Projects.tsx
+++ src/components/Landing/ProjectsSection/Projects.tsx
@@ -7,64 +7,64 @@ import project3 from "public/images/portfolio22.png";
 import project4 from "public/images/portfolio20.png";
 
 export default function Projects() {
-	const project1content =
-		"Built a website for NAPS society, BIT Mesra. The website featured member login and creation of new members.";
-	const project2content =
-		"This project aims to connect developers or college students with common interests, on the basis of common interests, skills and college. Also built a Visual Studio Code extenion for the chat component of the project. Got to top 10 at IEEE Megaproject";
-	const project3content =
-		"This is the code for the portfolio website you're on right now";
-	const project4content =
-		"This is my first website, built during IEEE Lead 2.0. Got 3rd postion at the event for portfolio building project.";
-	return (
-		<div className={style.Projects}>
-			<OverlappingText
-				backgroundtext="Projects"
-				toptext="Check out what i have worked on"
-			/>
-			<div className="a10vhgap"></div>
-			<ProjectCard
-				content={project1content}
-				github="https://github.com/lohit244/naps-website"
-				image={project1}
-				projecturl="https://naps-website.vercel.app"
-				title="Naps Website"
-				tags={["NextJs", "ExpressJs", "MongoDB", "Typescript"]}
-			/>
-			<div className="a10vhgap"></div>
-			<ProjectCard
-				content={project2content}
-				github="https://github.com/lohit244/vsbuddies-website"
-				image={project2}
-				projecturl="https://vsbuddies.netlify.app"
-				title="VS Buddies"
-				reverse
-				tags={[
-					"ReactJs",
-					"ExpressJs",
-					"MongoDB",
-					"VSCode Extension",
-					"Firebase",
-				]}
-			/>
-			<div className="a10vhgap"></div>
-			<ProjectCard
-				content={project3content}
-				github="https://github.com/lohit244/portfolio22"
-				image={project3}
-				projecturl="https://lohit244.netlify.app"
-				title="Portfolio Project '22"
-				tags={["NextJs", "Typescript", "TailwindCSS"]}
-			/>
-			<div className="a10vhgap"></div>
-			<ProjectCard
-				content={project4content}
-				github="https://github.com/lohit244/4"
-				image={project4}
-				projecturl="https://lohit244.github.io/4"
-				title="Portfolio Project '20"
-				reverse
-				tags={["ReactJS"]}
-			/>
-		</div>
-	);
+  const project1content =
+    "Built a website for NAPS society, BIT Mesra. The website featured member login and creation of new members.";
+  const project2content =
+    "This project aims to connect developers or college students with common interests, on the basis of common interests, skills and college. Also built a Visual Studio Code extenion for the chat component of the project. Got to top 10 at IEEE Megaproject";
+  const project3content =
+    "This is the code for the portfolio website you're on right now";
+  const project4content =
+    "This is my first website, built during IEEE Lead 2.0. Got 3rd postion at the event for portfolio building project.";
+  return (
+    <div className={style.Projects}>
+      <OverlappingText
+        backgroundtext="Projects"
+        toptext="Check out what i have worked on"
+      />
+      <div className="a10vhgap"></div>
+      <ProjectCard
+        content={project1content}
+        github="https://github.com/lohit244/naps-website"
+        image={project1}
+        projecturl="https://naps-website.vercel.app"
+        title="Naps Website"
+        tags={["NextJs", "ExpressJs", "MongoDB", "Typescript"]}
+      />
+      <div className="a10vhgap"></div>
+      <ProjectCard
+        content={project2content}
+        github="https://github.com/lohit244/vsbuddies-website"
+        image={project2}
+        projecturl="https://vsbuddies.netlify.app"
+        title="VS Buddies"
+        reverse
+        tags={[
+          "ReactJs",
+          "ExpressJs",
+          "MongoDB",
+          "VSCode Extension",
+          "Firebase",
+        ]}
+      />
+      <div className="a10vhgap"></div>
+      <ProjectCard
+        content={project3content}
+        github="https://github.com/lohit244/portfolio22"
+        image={project3}
+        projecturl="https://lohitaksha.com"
+        title="Portfolio Project '22"
+        tags={["NextJs", "Typescript", "TailwindCSS"]}
+      />
+      <div className="a10vhgap"></div>
+      <ProjectCard
+        content={project4content}
+        github="https://github.com/lohit244/4"
+        image={project4}
+        projecturl="https://lohit244.github.io/4"
+        title="Portfolio Project '20"
+        reverse
+        tags={["ReactJS"]}
+      />
+    </div>
+  );
 }
